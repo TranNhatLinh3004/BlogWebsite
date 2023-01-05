80616c0fe336cbd402bdc40a31a39a140b3a16ff
Trần Nhật Linh <linh54977@donga.edu.vn>
2023-01-05 21:46:23 +0700

    Cập nhập file auth.js trong folder phần login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,33 +1,13 @@
 const router = require("express").Router();
-const { handleRegister } = require("../controllers/index.controller");
+const {
+    handleRegister,
+    handleLogin,
+} = require("../controllers/index.controller");
 
-// regsiter
+// register;
 router.post("/register", handleRegister);
 
+//login
+router.post("/login", handleLogin);
+
 module.exports = router;
-
-// ---------------
-// const router = require("express").Router();
-// const User = require("../models/User");
-// const bcrypt = require("bcrypt");
-
-// // regsiter
-// router.post("/register", async(req, res) => {
-//     try {
-//         const salt = await bcrypt.genSalt(10);
-//         const hashedPass = await bcrypt.hash(req.body.password, salt);
-
-//         const newUser = new User({
-//             username: req.body.username,
-//             email: req.body.email,
-//             password: hashedPass,
-//         });
-
-//         const user = await newUser.save();
-//         res.status(200).json(user);
-//     } catch (error) {
-//         res.status(500).json(error);
-//     }
-// });
-
-// module.exports = router;
